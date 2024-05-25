7e136aa531e142d7e1b065b109107e581aa2264a
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-05-25 21:39:19 +0200

    add console to sitemap

--- app/[locale]/sitemap.ts
+++ app/[locale]/sitemap.ts
@@ -1,43 +1,55 @@
 import { MetadataRoute } from "next";
 
+const baseUrl = process.env.API_URL || "https://webmart-sigma.vercel.app";
+const languages = ["en", "ar", "fr"];
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = process.env.API_URL;
-  const languages = ["en", "ar", "fr"];
+  try {
+    const allProducts = await Promise.all(
+      languages.map(async (lang) => {
+        const response = await fetch(`${baseUrl}/${lang}/product`);
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch products for ${lang}: ${response.statusText}`
+          );
+        }
+        const products = await response.json();
+        return { lang, products };
+      })
+    );
 
-  if (!baseUrl) {
-    throw new Error("API_URL environment variable is not defined");
-  }
+    const productEntries: MetadataRoute.Sitemap = allProducts.flatMap(
+      ({ lang, products }) =>
+        products.map(({ id }: any) => ({
+          url: `${baseUrl}/${lang}/products/${id}`,
+          alternates: languages.reduce((acc, lang) => {
+            acc[lang] = `${baseUrl}/${lang}/products/${id}`;
+            return acc;
+          }, {} as { [key: string]: string }),
+        }))
+    );
 
-  const response = await fetch(`${baseUrl}/product`);
-  const products = await response.json();
-
-  const productEntries: MetadataRoute.Sitemap = products
-    .map(({ id }: any) => {
-      return languages.map((language) => ({
-        url: `${baseUrl}/${language}/products/${id}`,
+    const staticEntries: MetadataRoute.Sitemap = languages.flatMap((lang) =>
+      [
+        "",
+        "products",
+        "edit-profile",
+        "dashboard",
+        "cart",
+        "login",
+        "register",
+      ].map((path) => ({
+        url: `${baseUrl}/${lang}/${path}`,
         alternates: languages.reduce((acc, lang) => {
-          acc[lang] = `${baseUrl}/${lang}/products/${id}`;
+          acc[lang] = `${baseUrl}/${lang}/${path}`;
           return acc;
         }, {} as { [key: string]: string }),
-      }));
-    })
-    .flat();
+      }))
+    );
 
-  const staticEntries: MetadataRoute.Sitemap = [
-    "",
-    "products",
-    "edit-profile",
-    "dashboard",
-    "cart",
-    "login",
-    "register",
-  ].map((path) => ({
-    url: `${baseUrl}/en/${path}`,
-    alternates: languages.reduce((acc, lang) => {
-      acc[lang] = `${baseUrl}/${lang}/${path}`;
-      return acc;
-    }, {} as { [key: string]: string }),
-  }));
-
-  return [...staticEntries, ...productEntries];
+    return [...staticEntries, ...productEntries];
+  } catch (error) {
+    console.error("Error generating sitemap:", error);
+    throw new Error("Error generating sitemap");
+  }
 }
